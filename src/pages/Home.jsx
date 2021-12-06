46155f52014fa5d8ee184a8379c69ae503297fe3
murkhunting <marcmp.arq@gmail.com>
2021-12-06 16:03:12 +0100

    basic frontend structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./home.scss"
+import Navbar from "../components/navbar/Navbar"
+
+const Home = () => {
+    return (
+        <div className="home">
+            Homepage
+            <Navbar/>
+    
+        </div>
+    )
+}
+
+export default Home

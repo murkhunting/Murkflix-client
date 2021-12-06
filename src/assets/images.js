b3c2aa0b915ac44e0fb6ca15a8f7235f28e102ec
murkhunting <marcmp.arq@gmail.com>
2021-12-06 16:17:48 +0100

    add logo

---
+++ src/assets/images.js
@@ -0,0 +1,9 @@
+import logo1 from './img/Murkflixlogo.png'
+import logo2 from './img/Murkflixlogo2.png'
+
+const images = {
+    "logo1" : logo1,
+    "logo2" : logo2,
+}
+
+export default images;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./navbar.scss"
+import images from '../../assets/images'
 
 const Navbar = () => {
     return (
         <div className="navbar">
+            <img src={images.logo1} alt="" />
             Navbar
         </div>
     )

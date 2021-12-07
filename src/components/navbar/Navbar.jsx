c62ae77831d4bddd6627712327b28e08f8b106de
murkhunting <marcmp.arq@gmail.com>
2021-12-07 09:10:14 +0100

    navbar style finished

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./navbar.scss";
 import images from "../../assets/images";
+import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import { IoIosNotifications } from "react-icons/io";
 import { MdArrowDropDown } from "react-icons/md";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
           <img src={images.logo1} alt="" />
@@ -22,7 +30,13 @@ const Navbar = () => {
           <span>KID</span>
           <IoIosNotifications className="icon" />
           <img src={images.profile} alt="" />
-          <MdArrowDropDown className="icon" />
+          <div className="profile">
+            <MdArrowDropDown className="icon" />
+            <div className="options">
+              <span>SETTINGS</span>
+              <span>LOGOUT</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>

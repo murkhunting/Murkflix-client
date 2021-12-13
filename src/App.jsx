1c03387f6f0528d0c71c50c5af68e656dfee851a
murkhunting <marcmp.arq@gmail.com>
2021-12-13 22:42:53 +0100

    condicionals for authentification and paths

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,38 @@ import Home from "./pages//home/Home";
 import Watch from "./pages/watch/Watch";
 import SignUp from "./pages/signUp/SignUp";
 import Login from "./pages/login/Login";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/movies" element={<Home type="movies" />} />
-        <Route path="/series" element={<Home type="series" />} />
-        <Route path="/watch" element={<Watch />} />
+        <Route
+          exact
+          path="/"
+          element={user ? <Home /> : <Navigate to="/signup" />}
+        />
+        <Route
+          path="/signup"
+          element={!user ? <SignUp /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+        {user && (
+          <>
+            <Route path="/movies" element={<Home type="movies" />} />
+            <Route path="/series" element={<Home type="series" />} />
+            <Route path="/watch" element={<Watch />} />
+          </>
+        )}
       </Routes>
     </Router>
   );

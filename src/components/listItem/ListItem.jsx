a9601378bc4ebac02b61f84f319cf3b291111e63
murkhunting <marcmp.arq@gmail.com>
2021-12-14 16:51:12 +0100

    link to watch

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -7,6 +7,7 @@ import { IoAdd } from "react-icons/io5";
 import { AiOutlineLike } from "react-icons/ai";
 import { AiOutlineDislike } from "react-icons/ai";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const ListItem = ({ index, item }) => {
   const [isHovered, setIsHovered] = useState(false);
@@ -31,43 +32,45 @@ const ListItem = ({ index, item }) => {
   }, [item]);
 
   return (
-    <div
-      className="listItem"
-      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <img src={movie.img} alt="" />
-      {isHovered && (
-        <>
-          <ReactPlayer
-            url={movie.trailer}
-            width="100%"
-            height="150px"
-            autoPlay={true}
-            muted={true}
-            loop={true}
-            controls={false}
-            playing={true}
-          />
-          <div className="itemInfo">
-            <div className="icons">
-              <BsFillPlayFill className="icon" />
-              <IoAdd className="icon" />
-              <AiOutlineLike className="icon" />
-              <AiOutlineDislike className="icon" />
+    <Link to={{ pathname: "/watch", movie: movie }}>
+      <div
+        className="listItem"
+        style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <img src={movie.img} alt="" />
+        {isHovered && (
+          <>
+            <ReactPlayer
+              url={movie.trailer}
+              width="100%"
+              height="150px"
+              autoPlay={true}
+              muted={true}
+              loop={true}
+              controls={false}
+              playing={true}
+            />
+            <div className="itemInfo">
+              <div className="icons">
+                <BsFillPlayFill className="icon" />
+                <IoAdd className="icon" />
+                <AiOutlineLike className="icon" />
+                <AiOutlineDislike className="icon" />
+              </div>
+              <div className="itemInfoTop">
+                <span>{movie.duration}</span>
+                <span className="limit">+{movie.limit}</span>
+                <span>{movie.year}</span>
+              </div>
+              <div className="desc">{movie.description}</div>
+              <div className="genre">{movie.genre}</div>
             </div>
-            <div className="itemInfoTop">
-              <span>{movie.duration}</span>
-              <span className="limit">+{movie.limit}</span>
-              <span>{movie.year}</span>
-            </div>
-            <div className="desc">{movie.description}</div>
-            <div className="genre">{movie.genre}</div>
-          </div>
-        </>
-      )}
-    </div>
+          </>
+        )}
+      </div>
+    </Link>
   );
 };
 

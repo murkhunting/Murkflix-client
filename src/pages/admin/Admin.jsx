531d89bcc33d0b68917aec8fca1351698614cab8
murkhunting <marcmp.arq@gmail.com>
2021-12-14 23:31:50 +0100

    admin page style

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "./admin.scss";
+import images from "../../assets/images";
 
 function Admin() {
-  return <div></div>;
+  return (
+    <div className="admin">
+      <div className="top">
+        <div className="wrapper">
+          <img className="logo" src={images.logo1} alt="" />
+          <h1>MURKFLIX ADMIN PANEL</h1>
+        </div>
+      </div>
+      <div className="container">
+        <h2>EDIT ALL MOVIES</h2>
+        <button className="loginbutton">CREATE NEW MOVIE</button>
+        <h2>EDIT ALL LISTS</h2>
+        <button className="loginbutton">CREATE NEW LIST</button>
+      </div>
+    </div>
+  );
 }
 
 export default Admin;
